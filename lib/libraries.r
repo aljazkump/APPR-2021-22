56fe1d62afab9d3a6aebd75f26b38310777fd776
aljazkump <91609950+aljazkump@users.noreply.github.com>
2022-01-18 15:08:11 +0100

    Update libraries.r

--- lib/libraries.r
+++ lib/libraries.r
@@ -8,6 +8,12 @@ library(readr)
 library(dplyr)
 library(tibble)
 
+library(tidyverse) # za vizualizacijo podatkov
+library(gridExtra) # za prikaz vec grafov naenkrat
+library(reshape) # za poenostavljeno obdelavo histogramov
+library(rgdal) # za branje .SHP detotek
+library(maptools) # za risanje .SHP detotek
+
 options(gsubfn.engine="R")
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
