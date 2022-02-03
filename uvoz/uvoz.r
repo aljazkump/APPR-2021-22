b16eaf2ff1551d338b15fa64d6bbdfec14f87618
aljazkump <91609950+aljazkump@users.noreply.github.com>
2022-02-03 10:09:32 +0100

    Add files via upload

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,6 +1,17 @@
 # 2. faza: Uvoz podatkov
 
-sl <- locale("sl", decimal_mark=",", grouping_mark=".");
+sl <- locale("sl", decimal_mark=",", grouping_mark=".")
+
+Tabela <- function(a, b, value, detoteka) {
+  detoteka[a:b,1:length(detoteka)] %>%
+    pivot_longer(
+      cols = colnames(detoteka[a:b, 1:length(detoteka)])[-1],
+      names_to = "Kategorija",
+      values_to = value
+    ) %>%
+    filter(Kategorija %in% c("Mladi", "Zreli", "Stari")) %>%
+    arrange(Kategorija, Leto)
+}
 
 # Tabela, ki prikazuje stevilke odseljevanja v Evropske drzave 
 # glede na starost(mladi, zreli, stari)
@@ -10,25 +21,23 @@ Starost_Evropa <- read.csv(
   encoding = "Windows-1250"
 )
 
-drzave = c("Avstrija", "Hrvaska", "Nemcija", "Italija", "Svica", "Zdruzeno.kraljestvo")
-
-Starost_Evropa <- Starost_Evropa %>% select(Leto, drzave)
+Starost_Evropa_temp <- Starost_Evropa %>% select(Leto, Avstrija, Hrvaska, Svica, Italija, Zdruzeno.kraljestvo, Nemcija)
 
 Tabela_EU_Mladi <- function(drzava) {
-  Starost_Evropa[1:10, 1:length(Starost_Evropa)] %>% 
-    pivot_longer(cols = colnames(Starost_Evropa[1:10, 1:length(Starost_Evropa)])[-1],
+  Starost_Evropa_temp[1:10, 1:length(Starost_Evropa_temp)] %>% 
+    pivot_longer(cols = colnames(Starost_Evropa_temp[1:10, 1:length(Starost_Evropa_temp)])[-1],
                  names_to = "DRZAVE", values_to = "MLADI") %>% filter(DRZAVE == drzava)
 }
 
 Tabela_EU_Zreli <- function(drzava) {
-  Starost_Evropa[11:20, 1:length(Starost_Evropa)] %>% 
-    pivot_longer(cols = colnames(Starost_Evropa[11:20, 1:length(Starost_Evropa)])[-1],
+  Starost_Evropa_temp[11:20, 1:length(Starost_Evropa_temp)] %>% 
+    pivot_longer(cols = colnames(Starost_Evropa_temp[11:20, 1:length(Starost_Evropa_temp)])[-1],
                  names_to = "DRZAVE", values_to = "ZRELI") %>% filter(DRZAVE == drzava)
 }
 
 Tabela_EU_Stari <- function(drzava) {
-  Starost_Evropa[21:30, 1:length(Starost_Evropa)] %>% 
-    pivot_longer(cols = colnames(Starost_Evropa[11:30, 1:length(Starost_Evropa)])[-1],
+  Starost_Evropa_temp[21:30, 1:length(Starost_Evropa_temp)] %>% 
+    pivot_longer(cols = colnames(Starost_Evropa_temp[11:30, 1:length(Starost_Evropa_temp)])[-1],
                  names_to = "DRZAVE", values_to = "STARI") %>% filter(DRZAVE == drzava)
 }
 
@@ -51,7 +60,7 @@ starost_evropa <- full_join(full_join(w1,w2),w3)
 
 # Tabela, ki prikazuje stevilke odseljevanja v Evropske drzave 
 # v tem primeru skoraj za vsako drzavo Evrope                                        
-                      
+
 Evropa <- read.csv(
   "podatki/starost_EU.csv",
   encoding = "Windows-1250"
@@ -63,19 +72,17 @@ colnames(evropa) <- c("region", "SKUPNO")
 mapdata = left_join(map_data("world"), evropa, by="region") %>%
   filter(!is.na(SKUPNO));
 
- 
+
 # Tabela, ki prikazuje stevilke odseljevanja iz vsake obcine
- 
+
 Obcine_Odseljeni <- read.csv(
   "podatki/obcine.csv", 
   encoding = "Windows-1250"
 )
 
 Obcine_Odseljeni %>% select(X, Skupaj)
-colnames(Obcine_Odseljeni) <- c("OBCINE", "SKUPAJ")
+colnames(Obcine_Odseljeni) <- c("OB_UIME", "SKUPAJ")
 Obcine_Odseljeni <- Obcine_Odseljeni[1:212, 1:2]
-
-delez = df
 
 
 # Tabela za kasnejso vizualizacijo obcin na zemljevidu 
@@ -85,9 +92,9 @@ SIob <- readOGR("podatki/OB.shp", layer = "OB", encoding = "Windows-1250");
 SIob_fort <- SIob %>%
   fortify("region");
 
-SIob_fort <- left_join(SIob_fort, delez, by = "id");
-                                
-                                
+SIob_fort <- left_join(SIob_fort, Obcine_Odseljeni);
+
+
 # Tabela, ki prikazuje stevilke odseljevanja iz Slovenije 
 # glede na izobrazbo, spol in starost 
 
@@ -115,17 +122,6 @@ starost_izobrazba_spol = full_join(full_join(a1,a2),a3)
 
 # Tabele za vizualizacijo
 
-Tabela <- function(a, b, value, detoteka) {
-  detoteka[a:b,1:length(detoteka)] %>%
-    pivot_longer(
-      cols = colnames(detoteka[a:b, 1:length(detoteka)])[-1],
-      names_to = "Kategorija",
-      values_to = value
-    ) %>%
-    filter(Kategorija %in% c("Mladi", "Zreli", "Stari")) %>%
-    arrange(Kategorija, Leto)
-}
-
 Izseljevanje_Mladi_Moski <- starost_izobrazba_spol %>%
   pivot_longer(cols = names(.)[-1:-2]) %>% filter(Kategorija == "Mladi") %>%
   filter(name %in% c("OS_moski","SS_moski","VS_moski"))
@@ -141,14 +137,13 @@ Izseljevanje_Zreli_Moski <- starost_izobrazba_spol %>%
 Izseljevanje_Zrele_Zenske <- starost_izobrazba_spol %>%
   pivot_longer(cols = names(.)[-1:-2]) %>% filter(Kategorija == "Zreli") %>%
   filter(name %in% c("OS_zenske","SS_zenske","VS_zenske"))
-                                
+
 
 # Tabela, ki prikazuje stevilke odseljevanja iz Slovenije 
 # glede na starost in spol    
 
-Starost_Spol = read_csv(
+Starost_Spol <- read.csv(
   "podatki/starost_spol.csv",
-  locale = sl
 )
 
 # Tabele za vizualizacijo
@@ -170,30 +165,10 @@ Aktivnost = read.csv(
   encoding = "Windows-1250"
 );
 
-Zaposleni <- function(a,b,value) {
-  Aktivnost[a:b, 1:length(Aktivnost)] %>% select(Leto,Zaposleno) %>% 
-    pivot_longer(cols = colnames(Aktivnost[a:b, 1:2])[-1], names_to = "STATUS", values_to = value)          
-}
+Aktivnost$Status <- rep(c("Mladi", "Zreli", "Mlade", "Zrele"), each = 10)
 
-Nezaposleni <- function(a,b,value) {
-  Aktivnost[a:b, 1:length(Aktivnost)] %>% select(Leto,Nezaposleno) %>% 
-    pivot_longer(cols = colnames(Aktivnost[a:b, seq(1,3,2)])[-1], names_to = "STATUS", values_to = value)          
-}
+Aktivnost <- tibble(Aktivnost)
 
-Mladi_zaposleni = Zaposleni(1, 10, "MLADI")
-Zreli_zaposleni = Zaposleni(11, 20, "ZRELI")
-Mlade_zaposlene = Zaposleni(21, 30, "MLADE")
-Zrele_zaposlene = Zaposleni(31, 40, "ZRELE")
 
-aktivnost_temp1 = full_join(full_join(Mladi_zaposleni, Zreli_zaposleni) , full_join(Mlade_zaposlene, Zrele_zaposlene))
 
-Mladi_nezaposleni = Nezaposleni(1, 10, "MLADI")
-Zreli_nezaposleni = Nezaposleni(11, 20, "ZRELI")
-Mlade_nezaposlene = Nezaposleni(21, 30, "MLADE")
-Zrele_nezaposlene = Nezaposleni(31, 40, "ZRELE")
 
-aktivnost_temp2 = full_join(full_join(Mladi_nezaposleni, Zreli_nezaposleni) , full_join(Mlade_nezaposlene, Zrele_nezaposlene))
-
-aktivnost = full_join(akti_temp1, akti_temp2)
-
-                                
