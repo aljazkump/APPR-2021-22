f877ea2542fcf9c4624ce3823e7c29f1644ecced
aljazkump <91609950+aljazkump@users.noreply.github.com>
2022-06-16 21:36:24 +0200

    Add files via upload

---
+++ lib/libraries.R
@@ -0,0 +1,23 @@
+library(knitr)
+library(rvest)
+library(gsubfn)
+library(tidyr)
+library(tmap)
+library(shiny)
+library(readr)
+library(dplyr)
+library(tibble)
+
+library(ggdendro)
+library(XML)
+library(xml2)
+library(methods)
+library(tidyverse) # za vizualizacijo podatkov
+library(gridExtra) # za prikaz vec grafov naenkrat
+library(reshape) # za poenostavljeno obdelavo histogramov
+library(rgdal) # za branje .SHP detotek
+library(maptools) # za risanje .SHP detotek
+library(ggalt)
+library(ggrepel)
+
+options(gsubfn.engine="R")
